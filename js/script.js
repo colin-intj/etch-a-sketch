f355fd15a6ba33fc33e22a504bfdc043ce210fcf
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-30 15:02:27 -0500

    Add 16×16 drawing area to page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const drawingArea = document.createElement('section');
+drawingArea.className = 'drawing-area';
+
+// Creates 256 divs to represent pixels in a 16×16 drawing area grid
+for (let i = 0; i < 256; i++) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  drawingArea.appendChild(pixel);
+}
+
+document.body.appendChild(drawingArea);
