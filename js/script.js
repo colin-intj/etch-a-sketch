69060f64803f5bc4f93c982d7362022de7366b1a
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-07-07 16:53:18 -0500

    Add JS class for creating new drawing areas
    To eventually resize the drawing area, the program will have to be able
    to create new ones. To make it easier to do so, this commit adds a
    JavaScript class for creating new drawing areas.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 'use strict';
 
-const drawingArea = document.createElement('table');
-drawingArea.className = 'drawing-area';
+/**
+ * Creates an HTML table representing a drawing area for creating pixel art. The
+ * `table` element is given a class of `drawing-area`, and each `td` element is
+ * given a class of `pixel`. Colored pixels are given a class of `colored`.
+ */
+class DrawingArea {
+  /**
+   * @param {number} rows - Number of rows that the drawing area will have
+   * @param {number} columns - Number of columns that the drawing area will have
+   */
+  constructor(rows, columns) {
+    this.rows = rows;
+    this.columns = columns;
 
-for (let rowNumber = 0; rowNumber < 16; rowNumber++) {
-  const row = document.createElement('tr');
+    const element = document.createElement('table');
+    element.className = 'drawing-area';
 
-  for (let columnNumber = 0; columnNumber < 16; columnNumber++) {
-    const pixel = document.createElement('td');
-    pixel.className = 'pixel';
-    pixel.onclick = () => pixel.classList.toggle('colored');
+    for (let rowNumber = 0; rowNumber < rows; rowNumber++) {
+      const row = document.createElement('tr');
 
-    row.appendChild(pixel);
+      for (let columnNumber = 0; columnNumber < columns; columnNumber++) {
+        const pixel = document.createElement('td');
+        pixel.className = 'pixel';
+        pixel.onclick = () => pixel.classList.toggle('colored');
+
+        row.appendChild(pixel);
+      }
+
+      element.appendChild(row);
+    }
+
+    this.element = element;
   }
-
-  drawingArea.appendChild(row);
 }
 
-document.querySelector('.drawing-area-container').appendChild(drawingArea);
+const drawingArea = new DrawingArea(16, 16);
+
+document.querySelector('.drawing-area-container')
+    .appendChild(drawingArea.element);
